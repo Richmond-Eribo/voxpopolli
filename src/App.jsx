2c5fba969107095883fc4bddfd965573d7845936
Richmond Eribo <uyiosaeribo344@gmail.com>
2022-07-15 10:39:38 +0100

    updating SEO

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,25 @@ import Candidates from './pages/Candidates'
 import Candidate from './pages/Candidate'
 import {Hamburger} from './components'
 import SingleBlog from './pages/SingleBlog'
+import {Helmet} from 'react-helmet'
 
 function App() {
   return (
     <Router>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Voxpopuli Nigeria</title>
+        <meta
+          og:name='description'
+          k
+          content='Positively impacting voting decisions of the people by presenting
+      the public with hard facts about presidential aspirants'
+        />
+        <link rel='canonical' href='https://voxpopuli-nig.com' />
+        <meta property='og:title' />
+        <meta property='og:image' key='og_image' content='./assets/logo.png' />
+      </Helmet>
+
       <Hamburger />
       <Routes>
         <Route path='/' element={<Home />} />
